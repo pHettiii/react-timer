d9c9c4af61995cbcede63e570e66e29f84467029
pHettiii <madsdos@live.dk>
2021-02-22 18:29:50 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Tid from "./tid.js";
+import reportWebVitals from './reportWebVitals';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function ClockOne() {
+    return new Tid(1);
+}
+function ClockTwo() {
+    return new Tid(2);
+}
+function ClockThree() {
+    return new Tid(3);
+}
+function ClockFour() {
+    return new Tid(4);
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+      <div className={'watches'}>
+      <ClockOne />
+      <ClockTwo />
+      <ClockThree />
+      <ClockFour />
+      </div>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
+reportWebVitals();
 serviceWorker.unregister();
